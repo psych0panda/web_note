6afea0046a1998309d5c6f602b66217893559b61
sixth_fox <i.pyatigorets@gmailcom>
2018-07-03 08:38:36 +0300

    change codesample plugin

--- blog/static/blog/js/tinymce/custom.js
+++ blog/static/blog/js/tinymce/custom.js
@@ -6,26 +6,23 @@ tinymce.init({
         });
         },
   height: 500,
-  menubar: 'tools',
   plugins: [
     'advlist autolink lists link image charmap print preview anchor textcolor',
     'searchreplace visualblocks code fullscreen',
     'insertdatetime media table contextmenu paste code help wordcount',
-    'codesample',
+    'codesample code',
   ],
-  toolbar: 'insert | undo redo | codesample  |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
-    codesample_languages: [
-        {text: 'Python', value: 'python'},
+  codesample_languages: [
         {text: 'HTML/XML', value: 'markup'},
-        {text: 'JavaScript', value: 'javascript'},
         {text: 'CSS', value: 'css'},
-        {text: 'Http', value: 'http'},
-        {text: 'Django/Jinja2', value: 'django/jinja2'},
-        {text: 'nginx', value: 'nginx'},
         {text: 'C', value: 'c'},
+        {text: 'JavaScript', value: 'javascript'},
+        {text: 'Git', value: 'git'},
+        {text: 'Json', value: 'json'},
         {text: 'SQL', value: 'sql'},
-        {text: 'Git', value: 'git'}
+        {text: 'Python', value: 'python'},
     ],
+  toolbar: 'insert | undo redo | codesample code  |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
   content_css: [
     '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
     '//www.tinymce.com/css/codepen.min.css'],

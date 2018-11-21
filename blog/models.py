7e245a44c4f13fec14bd374b6844de42e6947e5a
sixth-fox <i.pyatigorets@gmail.com>
2018-11-21 13:41:58 +0200

    reformat

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from tinymce import HTMLField
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)

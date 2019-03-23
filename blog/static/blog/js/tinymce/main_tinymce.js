eb7fae73950b0ee4975619025b283468df2a122c
psych0 <i.pyatigorets@gmail.com>
2019-03-23 08:46:29 +0200

    remove static files

---
+++ blog/static/blog/js/tinymce/main_tinymce.js
@@ -0,0 +1,28 @@
+tinymce.init({
+  selector: 'textarea',
+  setup: function (editor) {
+        editor.on('change', function () {
+            editor.save();
+        });
+        },
+  height: 500,
+  plugins: [
+    'advlist autolink lists link image charmap print preview anchor textcolor',
+    'searchreplace visualblocks fullscreen',
+    'insertdatetime media table contextmenu paste help wordcount',
+    'codesample code',
+  ],
+  codesample_languages: [
+        {text: 'HTML/XML', value: 'markup'},
+        {text: 'CSS', value: 'css'},
+        {text: 'C', value: 'c'},
+        {text: 'JavaScript', value: 'javascript'},
+        {text: 'Git', value: 'git'},
+        {text: 'Json', value: 'json'},
+        {text: 'SQL', value: 'sql'},
+        {text: 'Python', value: 'python'},
+    ],
+  toolbar: 'insert | undo redo | codesample code  |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+  content_css: [
+    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+    '//www.tinymce.com/css/codepen.min.css']})

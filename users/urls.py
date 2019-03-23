383eab133f6a9ba9ea3150c787a3833282ecad32
psych0 <i.pyatigorets@gmail.com>
2019-03-23 23:50:44 +0200

    It seems this detail is superfluous, oh, why did it all break?

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from django.contrib.auth.views import auth_login
+from django.contrib.auth import login
 
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', auth_login, {'template_name': 'users/login.html'}, name='login'),
+    path('login/', login, {'template_name': 'users/login.html'}, name='login'),
     path('logout', views.logout_users, name='logout'),
 
 ]
